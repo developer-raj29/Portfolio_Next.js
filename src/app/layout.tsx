2c3f2b5ca4210fc926ac4e20b22238038a247420
Raj Yadav <rajyadav7047@gmail.com>
2024-10-13 16:09:05 +0530

    add preloader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -5,11 +6,13 @@ import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 // import { BackgroundBeams } from "@/components/UI_Components/background-beams";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useState } from "react";
+import Loading from "../components/Sub_Component/Loading";
 // import ContactInfo from "@/components/Sub_Component/ContactInfo";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
   title: "Developer | Raj🧑🏻",
   description: "I'm a fullstack developer",
 };
@@ -19,16 +22,30 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+        setLoading(false);
+    },4000);
+  }, []);
+
   return (
     <html lang="en">
       <body className={inter.className}>
         <main className="w-screen h-screen overflow-x-hidden mx-auto">
-          <div className="relative flex justify-center w-[95%] mx-auto">
-            <NavBar />
-          </div>
-          {children}
-          {/* <BackgroundBeams /> */}
-          <Footer />
+          {loading ? (
+            <Loading />
+          ) : (
+            <>
+              <div className="relative flex justify-center w-[95%] mx-auto">
+                <NavBar />
+              </div>
+              {children}
+              {/* <BackgroundBeams />*/}
+              <Footer />
+            </>
+          )}
         </main>
       </body>
     </html>
